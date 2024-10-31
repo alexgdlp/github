15dfb063607e4f00582a9225109a5c72c8caac25
alexgdlp <alex.unx@gmail.com>
2024-10-31 12:02:33 +0100

    Canvis

--- github.R
+++ github.R
@@ -3,7 +3,9 @@
 
 library(usethis)
 
-# Establir connexió amb GitHub
+# Establir connexió amb GitHub per primera vegada
+
+# Configuració inicial
 use_git_config(user.name="alexgdlp_laptop",    # Local; not GitHub
                user.email="alex.unx@gmail.com" # GitHub
                )
@@ -13,8 +15,17 @@ git_default_branch_configure()
 create_github_token()    # 1. Genera token
 gitcreds::gitcreds_set() # 2. Introduïr token generat
 gitcreds::gitcreds_get(use_cache=FALSE)$password # Optional: see tokens
+# /Establir connexió amb GitHub per primera vegada
 
-# Add current environment to GIT
+# Add current environment to GIT (fer per cada nou projecte)
+
+# Crear control de versió local
 use_git()
 
+# Pujar el control de versió local al GitHub
+# /Add current environment to GIT (fer per cada nou projecte)
+
+use_github()
+
+# Script substantiu
 print("hola món")
