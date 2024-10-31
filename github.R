1ce46f991f44c2b5171372a858681dbcb7ca3cd1
alexgdlp <alex.unx@gmail.com>
2024-10-31 11:38:43 +0100

    Initial commit

---
+++ github.R
@@ -0,0 +1,18 @@
+
+
+
+library(usethis)
+
+# Establir connexió amb GitHub
+use_git_config(user.name="alexgdlp_laptop",    # Local; not GitHub
+               user.email="alex.unx@gmail.com" # GitHub
+               )
+git_default_branch_configure()
+
+# Tokens are temporary passwords to link Local with GitHub
+create_github_token()    # 1. Genera token
+gitcreds::gitcreds_set() # 2. Introduïr token generat
+gitcreds::gitcreds_get(use_cache=FALSE)$password # Optional: see tokens
+
+# Add current environment to GIT
+use_git()
