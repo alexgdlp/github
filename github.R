041c4786983d9621b5ec65170ff1c5d8e5bff545
alexgdlp <alex.unx@gmail.com>
2024-10-31 12:36:11 +0100

    Fi de la segona sessió

--- github.R
+++ github.R
@@ -3,7 +3,7 @@
 
 library(usethis)
 
-# Establir connexió amb GitHub per primera vegada
+# A. Establir connexió amb GitHub per primera vegada
 
 # Configuració inicial
 use_git_config(user.name="alexgdlp_laptop",    # Local; not GitHub
@@ -14,20 +14,29 @@ git_default_branch_configure()
 # Tokens are temporary passwords to link Local with GitHub
 create_github_token()    # 1. Genera token
 gitcreds::gitcreds_set() # 2. Introduïr token generat
-gitcreds::gitcreds_get(use_cache=FALSE)$password # Optional: see tokens
-# /Establir connexió amb GitHub per primera vegada
+gitcreds::gitcreds_get(use_cache=FALSE)$password # Optional: see token
 
-# Add current environment to GIT (fer per cada nou projecte)
+# /A. Establir connexió amb GitHub per primera vegada
+
+# B. Add current environment to GIT (fer per cada nou projecte)
 
 # Crear control de versió local
 use_git()
 
 # Pujar el control de versió local al GitHub
-# /Add current environment to GIT (fer per cada nou projecte)
+# /B. Add current environment to GIT (fer per cada nou projecte)
 
 use_github()
 
-# Script substantiu
+# C. Projecte de GitHub forked a RStudio
+create_from_github("https://github.com/alexgdlp/proves_git.git",
+                   destdir="C:/Users/Alex/Desktop/github/fork"
+                   
+)
+# /C. Projecte de GitHub forked a RStudio
+
+# D. Script substantiu
 print("hola món")
 
 print("necessito ajuda")
+# /D. Script substantiu
